d3364e331c750930910d8e0f4b9b5bbb8af99423
Charles Swaleh <charlswall@live.com>
2020-03-23 09:54:46 +0300

    create the etch-a-sketch function

--- js/index.js
+++ js/index.js
@@ -21,11 +21,40 @@ function defaultGame(gridNumber) {
 		container.appendChild(divs);
 
 		//add the event listener
-		for (let x = 0; x < document.querySelectorAll(".divs").length; x++){
-			document.querySelectorAll(".divs")[x].addEventListener("mouseover", function(){
+		for (let x = 0; x < document.querySelectorAll(".divs").length; x++) {
+			document.querySelectorAll(".divs")[x].addEventListener("mouseover", function() {
 				this.style.backgroundColor = "blue";
 			});
 		}
 	}
 }
 defaultGame(gridNumber);
+
+// Game 01 - Etch-a-Sketch
+
+playSketch.addEventListener("click", function() {
+	gridNumber = "";
+	container.innerHTML = "";
+	function sketchGame() {
+		gridNumber = parseInt(prompt("Enter gridSquares: "));
+		container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+		for (let i = 0; i < gridNumber * gridNumber; i++) {
+			const divs = document.createElement("div");
+			divs.classList.add("divs");
+			divs.style.display = "flex";
+			divs.style.justifyContent = "center";
+			divs.style.alignItems = "center";
+			divs.style.border = "1px solid black";
+			divs.style.borderRadius = "2px";
+			container.appendChild(divs);
+
+			// Add the event listener
+			for (let x = 0; x < document.querySelectorAll(".divs").length; x++){
+				document.querySelectorAll(".divs")[x].addEventListener("mouseover", function(){
+					this.style.backgroundColor = "green";
+				});
+			}
+		}
+	}
+	sketchGame();
+});
